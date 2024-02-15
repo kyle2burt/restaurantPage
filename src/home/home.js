ab19194faa14bb3bebc520ee65cf046e57c05e9a
Kyle Burt <kyle2burt@gmail.com>
2024-02-15 13:31:30 -0800

    Add home page for website
        - Loads home page from home.js

---
+++ src/home/home.js
@@ -0,0 +1,24 @@
+console.log("Hello from home.js");
+
+const intro = () => {
+    const content = document.querySelector("#content");
+
+    const intro = document.createElement("div");
+    const title = document.createElement("h1");
+    const quote = document.createElement("p");
+    const address = document.createElement("p");
+
+    intro.setAttribute("id", "intro");
+
+    title.textContent = "Kyle\'s Ramen";
+    quote.textContent = "\“In Japan, our favorite thing to do after drinking all night is to go to the ramen place at two or three in the morning\”"
+    address.textContent = "(1234 Ramen street #567 92071)";
+
+    intro.appendChild(title);
+    intro.appendChild(quote);
+    intro.appendChild(address);
+
+    content.appendChild(intro);
+}
+
+export default intro;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 console.log("Hello Webpack");
+
+console.log("Hello from index.js")
+
+// import your function
+import intro from './home/home';
+
+const content = document.querySelector("#content");
+
+intro(content);
